045d405b5888969e6bd1963373c38c3e200bb436
Max Rees <maxcrees@me.com>
2013-07-28 17:12:54 -0700

    gay

--- stan.py
+++ stan.py
@@ -3,7 +3,7 @@
 stan.py - Stan Kudzu, the markov chains bot
 Author: Seth Rees, www.sthrs.me
 """
-import socket, time, re, markov2, os, random
+import socket, time, re, markov3, os, random
 from config import *
 
 def sanatize(i):
@@ -11,21 +11,21 @@ def sanatize(i):
    return i
 
 def markov(text):
-   text = text.lower().replace('stan', '')
+   text = text.lower().replace('stan3', '')
    text = sanatize(text)
-   sentence = markov2.main(35, text)
+   sentence = markov3.main(35, text)
    sentence = sentence.strip()
+   time.sleep(3)
    say(sentence)
 
 irc = socket.socket()
 irc.connect((server, port))
-send = lambda text: irc.send(text.encode('utf-8'))
-send("NICK %s\n" % nickname)
-send("USER stan %s /dev/null :Stan Kudzu\n" % nickname)
+irc.send("NICK %s\n" % nickname)
+irc.send("USER stan %s /dev/null :Stan Kudzu\n" % nickname)
 scanner = r":(\S+)!\S+@(\S+) (\S+) (\S+) :(.+)"
 scan = re.compile(scanner)
-stan = re.compile(r"(?i)^[ ]*stan([,: !?.]|).*")
-say = lambda msg: send('PRIVMSG %s :%s\n' % (origin, msg.decode('utf-8')))   
+stan = re.compile(r"(?i)^(.* |)stan([,: !?.]|)( |$).*")
+say = lambda msg: irc.send('PRIVMSG %s :%s\n' % (origin, msg))
 msg = say
 if __name__ == "__main__":
    connected = True
@@ -38,26 +38,24 @@ while connected:
       break
    recv = recv.splitlines()
    for line in recv:
-      uline = line
-      line = line.decode('utf-8')
-      print (uline)
+      print line
       args = line.split()
       if line.strip().startswith("PING"):
          # Respond to pings
-         send("\nPONG %s\n" % args[1][1:])
+         irc.send("\nPONG %s\n" % args[1][1:])
          continue
       if "376 %s :" % nickname in line:
          # Identify
-         send('PRIVMSG NickServ :identify %s\n' % password)
+         irc.send('PRIVMSG NickServ :identify %s\n' % password)
          time.sleep(1)
-      if "266 %s :" % nickname in line:
+      if "376 %s :" % nickname in line:
          # Join channels
          if isinstance(channel, tuple) or isinstance(channel, list):
             for chan in channel:
-               try: send("JOIN %s\n" % chan)
+               try: irc.send("JOIN %s\n" % chan)
                except: pass
          else:
-            try: send("JOIN %s\n" % channel)
+            try: irc.send("JOIN %s\n" % channel)
             except: pass
       find = scan.search(line)
       if find:
@@ -69,30 +67,30 @@ while connected:
          origin = find.group(4)
          text = find.group(5).strip()
          buff = text.split()
-         print (nick)
-         if (text.startswith("Stan: die") or text.startswith("Stan: piss off")) and nick == owner:
-            send('QUIT :I spent an interesting evening recently with a grain of salt.\n')
+         if (text.lower().startswith("stan3: die") or text.lower().startswith("stan3: piss off")) and nick == owner:
+            irc.send('QUIT :I spent an interesting evening recently with a grain of salt.\n')
             connected = False
             break
-         if text.startswith("Stan: reload") and nick == owner:
+         if text.lower().startswith("stan3: reload") and nick == owner:
             say('Yes, sir! <o')
-            reload(markov2)
+            reload(markov3)
             continue
          else:
-            if not origin.startswith("#"):
-               print ("Writing")
+            choice = random.choice(range(0, 10))
+            if origin.startswith("#") and choice == 7:
+               print "Writing"
+               text = text.lower().replace('stan3:','').replace('stan3','')
                fobj = open("txt.txt", "a")
                fobj.write(text + '\n')
                fobj.close()
-               continue
-            elif re.search(stan, text):
-               print (text)
-               markov(text)
+            if 'stan3' in text.lower():
+               choice = 42
             else:
+               upper = 250
                # Decide whether to randomly say something or not
-               choice = random.randint(0,250)
-               if choice == 42:
-                  markov(text)
+               choice = random.randint(0,upper)
+            if choice == 42:
+               markov(text)
 while not connected:
-   input('\nDisconnected!')
+   raw_input('\nDisconnected!')
    exit(1)
