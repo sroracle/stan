a5eec57fe0a56d832c91214b52b75abd3be15199
sroracle <mr.max.rees@gmail.com>
2013-08-06 22:55:16 -0500

    Refactor and cleanup mod_calc

--- modules/calc.py
+++ modules/calc.py
@@ -1,26 +1,15 @@
 #!/usr/bin/env python3
 # coding=utf-8
-"""
-calc.py - Phenny Calculator Module
-Copyright 2008, Sean B. Palmer, inamidst.com
-Licensed under the Eiffel Forum License 2.
+import re, urllib.request, sys, codecs
+sys.stdout = codecs.getwriter('utf-8')(sys.stdout.buffer, 'strict')
 
-http://inamidst.com/phenny/
-"""
-
-import re, urllib.request, sys
-
+# Constants
+help = 'See http://futureboy.us/frinkdocs/'
+uri = 'http://futureboy.homeip.net/fsp/frink.fsp?fromVal='
 r_result = re.compile(r'(?i)<A NAME=results>(.*?)</A>')
 r_tag = re.compile(r'<\S+.*?>')
-
 subs = [
-#   (' in ', ' -> '),
-#   (' to ', ' -> '),
-#   (' as ', ' -> '),
-#   (' over ', ' / '),
    ('AU', 'au'),
-   ('£', 'GBP '), 
-   ('€', 'EUR '), 
    ('\$', 'USD '), 
    ('KB', 'kilobytes'),
    ('lb', 'pound'),
@@ -34,47 +23,31 @@ subs = [
    ('fl_oz', 'floz')
 ]
 
-def calc(input): 
-   """
-   {0}calc <expression...> - Use the Frink online calculator.
-   For commands, expressions, and examples, see http://futureboy.us/frinkdocs
-   """
-   q = ' '.join(input).strip()
-   if not q: 
-      return 'See http://futureboy.us/frinkdocs/'
+# Argument retrieval
+if len(sys.argv) < 1:
+   print(help)
+args = sys.argv[1:]
+args = ' '.join(args).strip()
+if not args:
+   print(help)
 
-   query = q[:]
-   for a, b in subs: 
-      query = re.sub(a, b, query)
-   query = query.rstrip(' \t')
+# Query parsing
+query = args[:]
+for a, b in subs:
+   query = re.sub(a, b, query)
+query = query.rstrip(' \t')
+query = urllib.request.quote(query.encode('utf-8'))
 
-   precision = 5
-   if query[-3:] in ('GBP', 'USD', 'EUR', 'NOK'): 
-      precision = 2
-   query = urllib.request.quote(query.encode('utf-8'))
-
-   uri = 'http://futureboy.homeip.net/fsp/frink.fsp?fromVal='
-   bytes = urllib.request.urlopen(uri + query).read().decode()
-   m = r_result.search(bytes)
-   if m: 
-      result = m.group(1)
-      result = r_tag.sub('', result) # strip span.warning tags
-      result = result.replace('&gt;', '>')
-      result = result.replace('(undefined symbol)', '(?) ')
-
-      if '.' in result: 
-         try: result = str(round(float(result), precision))
-         except ValueError: pass
-
-      if not result.strip(): 
-         result = '?'
-      elif ' in ' in q: 
-         result += ' ' + q.split(' in ', 1)[1]
-
-      return q + ' = ' + result[:350]
-   else: return "Sorry, can't calculate that."
-
-if len(sys.argv) > 1:
-   print(calc(sys.argv[1:]))
+# Scrape result
+scrape = urllib.request.urlopen(uri + query).read().decode()
+result = r_result.search(scrape)
+if result:
+   result = result.group(1)
+   result = r_tag.sub('', result)
+   result = result.replace('&gt;', '>')
+   result = result.replace('(undefined symbol)', '(?) ')
+   if not result.strip():
+      result = '?'
+   print(args + ' = ' + result[:350])
 else:
-   print(calc(''))
+   print("Sorry, can't calculate that.")
