ccf4dc4cf9076b11ba32f951c2fa0902caba793e
sroracle <mr.max.rees@gmail.com>
2013-08-19 16:14:17 -0500

    Show (... ago) in .seen and make it actually work

--- modules/seen.py
+++ modules/seen.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import sys, time
+from datetime import timedelta
 
 if len(sys.argv) < 2:
    sys.exit(1)
@@ -10,8 +11,11 @@ with open('data/seen.db', 'r') as f:
    for line in f.readlines():
       h, k = line.split()
       if h == nick:
-         k = time.localtime(int(k))
+         now = int(time.time())
+         k = int(k)
+         delta = timedelta(seconds=(now - k))
+         k = time.gmtime(k)
          k = time.strftime('%a %b %d %Y %I:%M:%S %p %Z', k)
-         print('I last saw {0} on {1}'.format(h, k))
+         print('I last saw {0} on {1} ({2} ago)'.format(h, k, delta))
          sys.exit(0)
 print('I have never seen {0}.'.format(nick))
